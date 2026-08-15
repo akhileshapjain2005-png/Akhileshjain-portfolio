af1b2c1ab91cc7a68e8178cdad4712aa26894977
akhileshapjain2005-png <akhileshapjain2005@gmail.com>
2026-08-15 20:44:31 +0530

    git page dep

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:'/Akhileshjain-portfolio/',
+  base:process.env.GITHUB_ACTIONS ?'/Akhileshjain-portfolio/' : '/',
 });
