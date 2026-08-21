71c78b5ecfa704da61b32d44e9a3818febc062e8
akhileshapjain2005-png <akhileshapjain2005@gmail.com>
2026-08-21 23:14:49 +0530

    fix certificate image paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:process.env.NODE_ENV === 'production'
+  base:process.env.GITHUB_ACTIONS
   ?'/Akhileshjain-portfolio/'
   :'/',
 });
